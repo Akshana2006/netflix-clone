7641dadc3a2c61e0cb08bab38ea53793d3e53654
Akshana2006 <akaashiwho@gmail.com>
2024-03-26 10:55:40 +0530

    trailer feature update

--- src/Nav.js
+++ src/Nav.js
@@ -1,13 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import netflixLogo from "./images/netflix-logo.png";
 import netflixAvatar from "./images/netflix-avatar.png";
 import "./Nav.css"; 
 
 function Nav() {
+    const [show, handleShow] = useState(false);
+
+    useEffect(() => {
+        window.addEventListener("scroll" , () => {
+            if(window.scrollY > 100) {
+                handleShow(true);
+            }   else handleShow(false);
+        });
+        return () => {
+            window.removeEventListener("scroll", null);
+        };
+    },[]);
+
   return (
-    <div className="nav__logo">
+    <div className={`nav $(show && "nav__black")`}>
         <img
-            className="nav_logo"
+            className="nav__logo"
             src={netflixLogo}
             alt="Netflix logo"
         />
--- src/Row.js
+++ src/Row.js
@@ -1,43 +1,69 @@
-import React, {useState, useEffect} from 'react'
-import axios from './axios';
-import './Row.css';
+import React, { useState, useEffect } from "react";
+import axios from "./axios";
+import "./Row.css";
+import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer";
 
-// where do u get this from? 
-const base_url =  "https://image.tmdb.org/t/p/original/";
+// where do u get this from?
+const base_url = "https://image.tmdb.org/t/p/original/";
 
-function Row({title, fetchUrl, isLargeRow }) {
-    const [movies, setMovies] = useState([]);
+function Row({ title, fetchUrl, isLargeRow }) {
+  const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState([]);
 
-    useEffect(() => {
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      console.log(request);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
 
-        async function fetchData() {
-            const request = await axios.get(fetchUrl);
-            console.log(request);
-            setMovies(request.data.results);
-            return request;
-        }
-        fetchData();
-    }, [fetchUrl]);
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
 
-    console.log(movies);
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl(" ");
+    } else {
+      movieTrailer(movie?.name || "")
+        .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get("v"));
+        })
+        .catch((error) => console.log(error));
+    }
+  };
+
+  console.log(movies);
 
   return (
     <div className="row">
-        <h2>{title}</h2>
+      <h2>{title}</h2>
 
-        <div className="row__posters">
-        {movies.map(movie => (
-          <img 
-          key={movie.id}
-          className={`row__poster ${isLargeRow && "row__posterLarge"}`}
-          src={`${base_url}${
-             isLargeRow ? movie.poster_path : movie.backdrop_path
-          }`} 
-          alt={movie.name}/>
+      <div className="row__posters">
+        {movies.map((movie) => (
+          <img
+            key={movie.id}
+            onClick={() => handleClick(movie)}
+            className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+            src={`${base_url}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
+            alt={movie.name}
+          />
         ))}
-        </div>
+      </div>
+      {trailerUrl && <YouTube videoID={trailerUrl} opts={opts} />}
     </div>
-  )
+  );
 }
 
-export default Row
+export default Row;
